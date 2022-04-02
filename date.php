838f2119fa31d794bb2d9d0d2666022ecfbe30d7
DESKTOP-0OBD3D5\star zz <mjunaidafzal395@gmail.com>
2022-04-02 00:35:55 -0700

    date.php

---
+++ date.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>date of php</title>
+</head>
+<body>
+    <?php
+    echo "<h2>PHP DATE AND TIME</h2> <br>";
+    echo "Today date is     ".date("Y-m-d")."<br>";
+    echo "The time is     ".date("h:i:sa");
+    ?>
+</body>
+</html>
